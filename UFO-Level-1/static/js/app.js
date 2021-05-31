571f491f24815a14c965e819bcc7d9977c4eb791
ShanRaye <shanna@the-greathouses.net>
2021-05-31 18:40:36 -0400

    Removed class from html table
    Trying to get the table to display rows

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,13 +1,13 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Create a table
 function showTable(tableData){
     var tbody = d3.select(".table").select("tbody");
     tbody.html("");
-    tableData.forEach((x) => {
+    tableData.forEach((tableData) => {
       var row = tbody.append("tr");
-      Object.entries(x).forEach(([key, value]) => {
+      Object.entries(tableData).forEach(([key, value]) => {
         var cell = row.append("td");
         cell.text(value);
       });
